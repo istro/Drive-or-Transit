b3a80ce8042b18d6e2490bc35edc47807b1ae138
Apprentice <apprentice@dbc05.devbootcamp.com>
2012-07-09 21:48:05 -0700

    comparison methods are A'GOOOOOO

--- google_api_wrapper.rb
+++ google_api_wrapper.rb
@@ -2,17 +2,34 @@ require 'rest-client'
 require 'json'
 
 module GoogleAPI
+  def shorter_duration(mode_one, mode_two)
+    # mode_one.time < mode_two.time ? mode_one.mode.to_sym : mode_two.mode.to_sym
+    [mode_one, mode_two].min{|a,b| a.time<=>b.time }.mode.to_sym
+  end
+
   class MapData
+    attr_reader :mode
+
     def initialize(opts = {})
       opts.fetch(:origin) { raise ArgumentError, "need origin" }
       opts.fetch(:destination) { raise ArgumentError, "need destination" }
       opts.fetch(:sensor) { opts[:sensor] = false }
+      @mode = opts.fetch(:mode) { "driving" }
       @opts = opts
     end
 
     def response
       into_json = RestClient.get "http://maps.googleapis.com/maps/api/directions/json", { :params => @opts }
       JSON.parse(into_json, :symbolize_names => true)
+    end
+
+    def distance
+      meters = self.response[:routes][0][:legs][0][:distance][:value]
+      miles = (meters * 6.214e-4).round(1)
+    end
+
+    def time
+      self.response[:routes][0][:legs][0][:duration][:value]
     end
   end
 end
--- google_api_wrapper_spec.rb
+++ google_api_wrapper_spec.rb
@@ -1,17 +1,19 @@
 require './google_api_wrapper'
+require 'rspec'
 
 include GoogleAPI
-describe "Google API Wrapper" do
+describe "MapData" do
+  # before(:each) do
+  #   @map_data = mock(MapData)
+  #   @map_data.stub!(:response).and_return(eval(File.read('map_data_mock.txt')))
+  # end
+
   let(:map_data) { MapData.new(origin: "San Francisco, CA", destination: "Sacramento, CA") }
 
   context "#new" do
     it "should accept required parameters: origin/destination/sensor" do
       lambda { MapData.new(opts = {}) }.should raise_error ArgumentError
     end
-
-    #it "should default sensor parameter to false" do
-      #map_data.sensor.should eq false
-    #end
   end
 
   context "JSON method" do
@@ -22,8 +24,36 @@ describe "Google API Wrapper" do
   end
 
   context "some method" do
-    it "should allow for optional parameters"
+    it "should allow for optional parameters" do
+      new_map_data = MapData.new(origin: "San Francisco, CA", destination: "Sacramento, CA", mode: "walking")
+      new_map_data.response.to_s.should =~ /:travel_mode=>"WALKING"/
+    end
   end
 
-  context "retrieve metrics"
+  context "#distance" do
+    it "contain return value of distance" do
+      map_data.distance.should eq 87.9
+    end
+  end
+
+  context "#time" do
+    it "should return a value of travel time" do
+      map_data.time.should eq 5954
+    end
+  end
+end
+
+describe "API Wrapper Module" do
+  before(:each) do
+    @transit = MapData.new(origin: "San Francisco, CA", destination: "Sacramento, CA", mode: "transit")
+    @driving = MapData.new(origin: "San Francisco, CA", destination: "Sacramento, CA")
+  end
+
+  it "compares duration" do
+    shorter_duration(@transit, @driving).should eq :driving
+  end
+
+  it "compares distance" do
+    shorter_distance(@transit, @driving).should eq :driving
+  end
 end
