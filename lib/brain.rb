7a7fb9b882ebf0ad6308e353e6a8c12a1ace2b5b
Sherief Gharraph + Ivan Stroganov <apprentice+sg+is@devbootcamp.com>
2012-07-11 16:19:55 -0700

    fixed checking for user existence in db

--- lib/brain.rb
+++ lib/brain.rb
@@ -5,16 +5,9 @@ module Transport
   class Brain
     attr_reader :user
     def initialize(user)
-      @user = User.find_or_create_by_first_name_and_last_name(user.first_name, user.last_name)
+      @user = User.find(:first, :conditions => {:first_name => user.first_name, :last_name => user.last_name}) || User.create(:first_name => user.first_name, :last_name => user.last_name)
     end
 
-    def add_address(address_hash)
-      @user.addresses.create(address_hash)
-    end
-
-    def list_addresses
-      puts @user.addresses.inspect
-    end
 
   end
 end
