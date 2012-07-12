cf244599e2be93ec6eb990f9e08d838d47f7eb14
Sherief Gharraph + Ivan Stroganov <apprentice+sg+is@devbootcamp.com>
2012-07-11 17:16:40 -0700

    added decision method

--- lib/brain.rb
+++ lib/brain.rb
@@ -1,5 +1,6 @@
 require_relative 'address'
 require_relative 'user'
+require_relative 'trip'
 
 module Transport
   class Brain
@@ -9,14 +10,19 @@ module Transport
     end
 
     def list_addresses
+      sorted_addresses = @user.addresses.sort_by(&:created_at).reverse
       output = []
-      @user.addresses.each do |address|
-        output << address.to_string
+      sorted_addresses.each do |address|
+        output.push address.db_to_str
       end
       output
     end
 
-
+    def decision(origin, destination)
+      driving = Trip.new(:origin => origin, :destination => destination)
+      transit = Trip.new(:origin => origin, :destination => destination, :mode => "transit")
+      Trip.shorter_duration(driving, transit)
+    end
 
   end
 end
--- spec/brain_spec.rb
+++ spec/brain_spec.rb
@@ -8,6 +8,10 @@ describe Brain do
     @my_brain = Brain.new(ivan)
   end
 
+  after (:each) do
+    Address.destroy_all
+  end
+
   context '#new' do
     it 'instantiates with user object' do
       @my_brain.user.first_name.should eq 'ivan'
@@ -19,7 +23,7 @@ describe Brain do
         @my_brain.list_addresses.should be_an_instance_of Array
       end
 
-      it 'returns an array of strings represting addresses' do
+      it 'returns an array of strings representing addresses' do
         @my_brain.user.addresses.create(:street => "717 california st",
                                         :city => "san fran",
                                         :state => "CA",
@@ -30,14 +34,23 @@ describe Brain do
                                         :state => "CA",
                                         :zip => "94123")
         puts 'addresses:'
-        puts @my_brain.user.addresses
-        @my_brain.list_address[0].index("Webster").nil?.should be false
-        @my_brain.list_address[1].index("california").nil?.should be false
+        puts @my_brain.user.addresses[0].db_to_str
+        puts @my_brain.user.addresses[1].db_to_str
+        @my_brain.list_addresses[0].index("Webster").nil?.should be false
+        @my_brain.list_addresses[1].index("california").nil?.should be false
       end
 
     end
 
+  context '#decision' do
+    it 'takes two arguments' do
+      lambda {@my_brain.decision()}.should raise_error
+      lambda {@my_brain.decision("717 california st, san francisco, CA, 94108", "2006A Webster St, san francisco, CA, 94123")}.should_not raise_error
+    end
 
+    it 'returns a symbol' do
+      @my_brain.decision("717 california st, san francisco, CA, 94108", "2006A Webster St, san francisco, CA, 94123").should == :driving
+    end
 
   end
 
